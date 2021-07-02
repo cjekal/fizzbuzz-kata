cd4eacdd1956afa88e1639ace603b56012d7ccf6
Charles Jekal <cjekal@highlighttech.com>
2021-07-01 20:59:27 -0700

    making the Fizz case work

---
+++ src/main/java/com/example/demo/FizzBuzzService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FizzBuzzService {
+    public String getWord(int number) {
+        return "fizz";
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private FizzBuzzService fizzBuzzService;
+
 	@Test
-	void contextLoads() {
+	void GetsFizzIfDivisibleBy3() {
+		assertThat(fizzBuzzService.getWord(3)).isEqualTo("fizz");
+		assertThat(fizzBuzzService.getWord(6)).isEqualTo("fizz");
+		assertThat(fizzBuzzService.getWord(12)).isEqualTo("fizz");
 	}
 
 }
